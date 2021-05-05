b2a48caefb4cf5f55e4e3bf85c2a657366b667a1
emisow <73198326+emisow@users.noreply.github.com>
2021-05-04 21:11:55 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -15,8 +15,8 @@ class Client:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
 
-        msg = tkinter.TK()
-        msg = withdraw()
+        msg = tkinter.Tk()
+        msg.withdraw()
 
         self.nickname = simpledialog.askstring("Nickname", "Por favor escolha um apelido", parent=msg)
 
@@ -31,7 +31,7 @@ class Client:
         receive_thread.start()
 
     def gui_loop(self):
-        self.win = tkinter.TK()
+        self.win = tkinter.Tk()
         self.win.configure(bg="blue")
 
         self.chat_label = tkinter.Label(self.win, text="Chat:", bg="blue")
@@ -43,7 +43,7 @@ class Client:
         self.text_area.config(state= 'disabled')
 
         self.msg_label = tkinter.Label(self.win, text="Chat:", bg="blue")
-        self.msg_labell.config(font=("Arial", 12))
+        self.msg_label.config(font=("Arial", 12))
         self.msg_label.pack(padx=20, pady=5)
 
         self.input_area = tkinter.Text(self.win, height=3)
@@ -51,13 +51,13 @@ class Client:
 
         self.send_button = tkinter.Button(self.win, text ="Send", command = self.write)
         self.send_button.config(font=("Arial", 12))
-        self.send_button.pack(pack=20, pady=5)
+        self.send_button.pack(padx=20, pady=5)
 
         self.gui_done = True
 
         self.win.protocol("WM_DELETE_WINDOW", self.stop)       
 
-        self.win.mainLoop()
+        self.win.mainloop()
     
     def write(self):
         message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
@@ -90,5 +90,5 @@ class Client:
                 self.sock.close()
                 break
 
-Client = Client(host, port)
+client = Client(host, port)
 
